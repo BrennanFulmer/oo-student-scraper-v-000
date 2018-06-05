fd1d941bc88a082f4a9a7bc8b841e983dd91b38f
Brennan Fulmer <brennan.flmr@gmail.com>
2018-06-05 21:02:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
 
   def initialize(student_hash)
     student_hash.each { |key, value| self.send(("#{key}="), value) }
-    @@all << self
+    self.class.all << self
   end
 
   def self.create_from_collection(students_array)
