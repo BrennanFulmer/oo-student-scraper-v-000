a242872bcd24abd3b227639111f665f4213e1823
Brennan Fulmer <brennan.flmr@gmail.com>
2018-06-05 21:42:48 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -32,7 +32,7 @@ class Scraper
         profile_data[:linkedin] = url
       elsif url.include? 'github'
         profile_data[:github] = url
-      elsif url.match(/^[\/#\.]/) == nil && url.match(/.*facebook.*/i) == nil && 
+      elsif url.match(/^[\/#\.]/) == nil && url.match(/.*facebook.*/i) == nil && url.match(/.*instagram.*/i) == nil
         profile_data[:blog] = url
       end
     }
