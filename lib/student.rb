9245254da65b7d5c6455428d9042d04b1e0b81f3
Brennan Fulmer <brennan.flmr@gmail.com>
2018-06-05 20:59:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,10 +2,6 @@ class Student
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
   @@all = []
 
-  def self.all
-    @@all
-  end
-
   def initialize(student_hash)
     student_hash.each { |key, value| self.send(("#{key}="), value) }
     self.all << self
